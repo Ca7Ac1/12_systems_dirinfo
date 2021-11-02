3a65577493b76e52676e601619971d6e82eeeb1f
Ca7Ac1 <Ca7Ac1@gmail.com>
2021-11-01 22:13:44 -0400

    In progress work

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+
+int dir(char *path, char *format)
+{
+    int total = 0;
+
+    DIR *d = opendir(path);
+    struct dirent *entry = readdir(d);
+
+    while (entry)
+    {
+        char newPath[strlen(path) + strlen(entry->d_name) + 1];
+        strcpy(newPath, path);
+        strcat(newPath, entry->d_name);
+
+        if (entry->d_type == DT_DIR)
+        {
+            strcat(newPath, "/");
+
+            char newFormat = (strlen(format) + 1);
+            strcpy(newFormat, format);
+            strcat(newFormat, "\t");
+
+            total += dir(newPath, newFormat);
+        }
+        else
+        {
+            struct stat *s;
+            stat(newPath, s);
+
+            int size = s->st_size;
+            total += size;
+
+            printf("File: %s\tSize:%u\n", entry->d_name, size);
+        }
+
+        entry = readdir(d);
+    }
+
+    printf("Total size:%u\n", total);
+    closedir(d);
+}
+
+int main()
+{
+    dir("./", "");
+}
